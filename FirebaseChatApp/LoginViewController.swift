91fc08fb9f1c751a34e3994bfe65600d1284b34e
Cynthia Whitlatch <cawhitlatch2@gmail.com>
2016-07-07 21:07:49 -0700

    no message

---
+++ FirebaseChatApp/LoginViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LoginViewController.swift
+//  FirebaseChatApp
+//
+//  Created by Cynthia Whitlatch on 7/7/16.
+//  Copyright © 2016 Cynthia Whitlatch. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
