e15f380564894f19efa15f21cbb65e01e4786417
Cynthia Whitlatch <cawhitlatch2@gmail.com>
2016-07-07 20:20:39 -0700

    Initial Commit

---
+++ FirebaseChatApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirebaseChatApp
+//
+//  Created by Cynthia Whitlatch on 7/7/16.
+//  Copyright © 2016 Cynthia Whitlatch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
